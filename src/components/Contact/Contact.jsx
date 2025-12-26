1ff06dbcb318022a203cbee0d595472fa41631bb
Navendu07 <166049418+Navendu07@users.noreply.github.com>
2025-12-26 18:40:20 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_ehu8drk",  // Replace with your EmailJS Service ID
+        "service_0v0hanv",  // Replace with your EmailJS Service ID
         "template_gaddcm6",  // Replace with your EmailJS Template ID
         form.current,
         "LfTmN_ndV-9Ucsjoh"  // Replace with your EmailJS Public Key
